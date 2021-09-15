5328a0d338c441e985e60d886486621539c9da7b
solariyoung <solariyoung@gmail.com>
2021-09-15 10:03:26 +0800

    Update onekeyapprove.js

--- onekeyapprove.js
+++ onekeyapprove.js
@@ -42,7 +42,7 @@ async function main() {
         
         await utils.sign_and_send_transaction2(web3, private_key, data, utils.Rarity_contract_address)
         
-        await(5000)
+        await wait(5000)
     }
    
     
